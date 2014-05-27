56f5902f26f0915e60836d6af68f929ed54610d5
Bob Watson <rbwatson@uw.edu>
2014-05-26 22:23:27 -0700

    getting closer...

--- raspberryPi/bashScripts/baseServer.sh
+++ raspberryPi/bashScripts/baseServer.sh
@@ -4,6 +4,8 @@
 #  file services for the Raspbery Pi to later run
 #  as a web server.
 #
+#	curl --silent "http://<Server>/raspi/bashScripts/baseServer.sh" | sudo sh
+#
 #++++
 #  check for root access
 #----
@@ -18,17 +20,56 @@ fi
 echo "updating OS"
 apt-get update
 #++++
-#  TODO:
 #	update /etc/network/interfaces
 #	  to use fixed IP on Ethernet
+#     and DHCP on wireless
 #----
+echo "Configuring network interfaces"
+cat - > /etc/network/interfaces <<NETWORKIFACE
+auto lo
 
+iface lo inet loopback
+
+iface eth0 inet static
+address 192.165.1.64
+netmask 255.255.255.0
+gateway 192.165.1.1
+
+
+allow-hotplug wlan0
+iface wlan0 inet manual
+wpa-roam /etc/wpa_supplicant/wpa_supplicant.conf
+#iface default inet dhcp
+
+NETWORKIFACE
 #++++
-#  TODO:
 #	update  /etc/wpa_supplicant/wpa_supplicant.conf
 #	  to use wireless network and password configured for router
 #----
+echo "Configuring WiFi connections"
+cat - > /etc/wpa_supplicant/wpa_supplicant.conf <<WIFICONFIG
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
 
+network={
+ssid="IHS-Field1"
+psk="IhsOfMn2014"
+
+# Protocol type can be: RSN (for WP2) and WPA (for WPA1)
+proto=WPA
+
+# Key management type can be: WPA-PSK or WPA-EAP (Pre-Shared or Enterprise)
+key_mgmt=WPA-PSK
+
+# Pairwise can be CCMP or TKIP (for WPA2 or WPA1)
+pairwise=CCMP TKIP
+group=CCMP TKIP
+
+#Authorization option should be OPEN for both WPA1/WPA2 (in less commonly used are SHARED and LEAP)
+auth_alg=OPEN
+}
+
+WIFICONFIG
 
 #++++
 #  install NTFS for USB drives
@@ -49,12 +90,13 @@ cat - > /etc/dnsmasq.conf <<DNSMASQCONF
 # adapted from: Stephen Wood heystephenwood.com
 
 domain-needed
+
 # Set up your local domain here
+address=/raspi.local/192.165.1.64
+address=/www.raspi.local/192.165.1.64#80
+address=/ftp.raspi.local/192.165.1.64#21
+address=/sftp.raspi.local/192.165.1.64#22
 domain=raspi.local
-#resolv-file=/etc/resolv.dnsmasq
-min-port=4096
-#server=8.8.8.8
-#server=8.8.4.4
 
 # Max cache size dnsmasq can give us, and we want all of it!
 cache-size=10000
@@ -62,21 +104,20 @@ cache-size=10000
 #no DHCP from dnsmasq
 no-dhcp-interface=
 
-# Below are settings for dhcp. Comment them out if you dont want
-# dnsmasq to serve up dhcpd requests.
-# dhcp-range=192.168.0.100,192.168.0.149,255.255.255.0,1440m
-# dhcp-option=3,192.168.0.1
-# dhcp-authoritative
-
 DNSMASQCONF
 
 #++++
-#  TODO:
 #	configure /etc/resolv.conf to use:
 #		localhost
 #		server's fixed IP
 #----
+echo "Configuring nameservers"
+cat - > /etc/resolv.conf <<NAMESERVERS
+nameserver 192.165.1.64
+nameserver 192.165.1.1
+nameserver 8.8.8.8
 
+NAMESERVERS
 
 #++++
 #  restart service to use new settings
@@ -84,7 +125,7 @@ DNSMASQCONF
 echo "Restarting DNS server to pick up new configuration"
 service dnsmasq restart
 #++++
-#  restart service to use new settings
+#  Install DNS tools
 #----
 echo "Installing DNS tools"
 apt-get install -y dnsutils
